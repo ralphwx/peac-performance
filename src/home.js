78c5d5c73925d39403c3004089cab6b9a771298e
ralphwx <119894249+ralphwx@users.noreply.github.com>
2025-05-21 10:37:55 -0400

    Formatted header for desktop

--- src/home.js
+++ src/home.js
@@ -1,8 +1,23 @@
 
 import ReactDOM from "react-dom/client";
+import "./home.css";
 
 function Main() {
-    return <h1>Hello World</h1>
+    return <div className="background">
+        <div className="header">
+            <div className="header logo">Peac Performance</div>
+            <div className="header menuitem">Home</div>
+            <div className="header menuitem">Services</div>
+            <div className="header menuitem">Contact</div>
+            <div className="header menuitem">About</div>
+        </div>
+        <div className="section">
+            <h1>Home</h1>
+        </div>
+        <div className="footer">
+            
+        </div>
+    </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
