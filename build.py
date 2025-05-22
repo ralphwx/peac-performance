7a7ba4941f2cb8d66b2e478f75cb470b12eb581a
ralphwx <119894249+ralphwx@users.noreply.github.com>
2025-05-22 16:15:06 -0400

    Updated build script, rebuilt

--- build.py
+++ build.py
@@ -35,9 +35,10 @@ new_html = original_html.replace("/static", "./static")
 with open("./build/index.html", "w") as f:
     f.write(new_html)
 
-media_dir = "./docs/" + outdir
-if media_dir[-1] != "/": media_dir += "/"
-media_dir += "static/media/"
+#media_dir = "./docs/" + outdir
+#if media_dir[-1] != "/": media_dir += "/"
+#media_dir += "static/media/"
+media_dir = "./docs/static/media"
 os.system("cp ./build/static/media/* " + media_dir)
 os.system("mkdir -p ./docs/" + outdir)
 os.system("cp -r ./build/* ./docs/" + outdir)
