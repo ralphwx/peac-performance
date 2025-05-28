f5c6078055c48ed6d9a97c7c1bffc989db41c4a0
ralphwx <119894249+ralphwx@users.noreply.github.com>
2025-05-28 15:25:18 -0400

    Added about blurb to home page

--- src/home.js
+++ src/home.js
@@ -3,16 +3,88 @@ import ReactDOM from "react-dom/client";
 import "./home.css";
 import {Header} from "./header.js";
 import {Footer} from "./footer.js";
+import {useState, useEffect} from "react";
 
-function ServiceBox({service, img, description}) {
-    return <div className="service-box">
-        <img src={img} className="service-preview-img" />
-        <div className="service-header">{service}</div>
-        <div className="service-description">{description}</div>
-        <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
-            <button className="learn-more-button">Learn More</button>
-        </div>
+const about_threshold = 960;
+
+function AboutBlurb() {
+    return <div>
+       <p style={{fontSize: "1.2rem"}}>
+           Lucinda Huang (she/her/hers) is a Licensed Marriage and 
+           Family Therapist (LMFT) currently practicing in Michigan. She
+           enjoys working with families, couples, and individuals to 
+           improve their mental health, daily functioning, and 
+           well-being.
+       </p>
+       <p style={{fontSize: "1.2rem"}}>
+           In addition to her role as a therapist, Lucinda is also 
+           doctoral candidate in the Couple and Family Therapy program 
+           at Michigan State University. Her program of research focuses
+           on understanding parent-adult child relationships, promoting
+           intergenerational solidarity, and disseminating of 
+           evidence-based interventions for underrepresented and 
+           disadvantaged populations. 
+       </p>
+       <p style={{fontSize: "1.2rem"}}>
+           Prior to her doctoral program, Lucinda received her 
+           bachelor’s degree in Psychology from Wheaton College and 
+           master's degree in Marriage and Family Therapy from 
+           Northwestern University. 
+       </p>
     </div>
+}
+
+function AboutHome() {
+    const [displayState, setDisplayState] = useState("wide");
+                    
+    let updateState = () => {
+        if(window.innerWidth > about_threshold) {
+            setDisplayState("wide");
+        } else {
+            setDisplayState("medium");
+        }
+    }
+
+    useEffect(() => {
+        updateState();
+        window.addEventListener("resize", updateState);
+
+        return () => {
+            window.removeEventListener("resize", updateState);
+        }
+    }, []);
+    
+    if(displayState === "wide") {
+       return <div style={{
+           display: "flex", 
+           justifyContent: "center", 
+           gap: "4rem",
+           alignItems: "flex-start"
+       }}>
+           <div style={{flex: 1, display: "flex", justifyContent: "center", maxWidth: "400px"}}>
+               <img 
+                   src="/lucinda-home.png" 
+                   style={{width: "100%", height: "auto"}}
+               />
+           </div>
+           <div style={{flex: 1, display: "flex", flexDirection: "column", alignItems: "center", maxWidth: "600px"}}>
+               <h1 style={{fontSize: "3rem"}}>Lucinda Huang 黄瑶冲, M.S., LMFT</h1>
+               <AboutBlurb />
+               <div className="learn-more-button purple">Read More</div>
+           </div>
+       </div>
+    } else {
+        return <div>
+            <h1>Lucinda Huang 黄瑶冲, M.S., LMFT</h1>
+            <img 
+                src="/lucinda-home.png"
+                style={{width: "100%", height: "auto"}}
+            />
+            <AboutBlurb />
+            <div className="learn-more-button purple">Read More</div>
+        </div>
+    }
+    
 }
 
 function Main() {
@@ -33,14 +105,10 @@ function Main() {
                 <button className="learn-more-button">About me</button>
             </div>
         </div>
-        <div className="section" style={{backgroundColor: "#b3cde3", display: "none"}}>
-            <div className="section-h1">Services</div>
-            <div className="services-container">
-                <ServiceBox service={"Individual Therapy"} img={"/individual.jpg"} description={"I offer individual therapy ..."} />
-                <ServiceBox service={"Couples Therapy"} img={"/couple.jpg"} description={"I offer individual therapy ..."} />
-                <ServiceBox service={"Family Therapy"} img={"/family.jpg"} description={"I offer individual therapy ..."} />
+        <div style={{backgroundColor: "#b3cde3", padding: "min(10%, 4rem)"}}>
+            <div style={{backgroundColor: "#c6dbef", padding: "min(10%, 3rem)"}}>
+                <AboutHome />
             </div>
-            <div style={{height: "5rem"}}></div>
         </div>
         <Footer />
     </div>
